5c2d7280628d06ab9a7de478ce44391ee9ab81cb
TomLBZ <libozhao142857@hotmail.com>
2020-08-30 18:48:09 +0800

    Add project files.

---
+++ CountersQueuingProblemSolver/Form1.Designer.cs
@@ -0,0 +1,404 @@
+﻿namespace CountersQueuingProblemSolver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtArrival = new System.Windows.Forms.TextBox();
+            this.lblArrival = new System.Windows.Forms.Label();
+            this.lblLevel = new System.Windows.Forms.Label();
+            this.txtLevels = new System.Windows.Forms.TextBox();
+            this.lblLvlNum = new System.Windows.Forms.Label();
+            this.txtLvlNum = new System.Windows.Forms.TextBox();
+            this.lblProcTime = new System.Windows.Forms.Label();
+            this.txtProcTime = new System.Windows.Forms.TextBox();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.lblPplInSys = new System.Windows.Forms.Label();
+            this.txtPplNum = new System.Windows.Forms.TextBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnSimulate = new System.Windows.Forms.Button();
+            this.lblInitial = new System.Windows.Forms.Label();
+            this.txtIniState = new System.Windows.Forms.TextBox();
+            this.lblStudentNum = new System.Windows.Forms.Label();
+            this.txtNumStudent = new System.Windows.Forms.TextBox();
+            this.lblQ = new System.Windows.Forms.Label();
+            this.txtQ = new System.Windows.Forms.TextBox();
+            this.lblStuYetCome = new System.Windows.Forms.Label();
+            this.txtYet = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnSimulateOnce = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(397, 426);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtArrival
+            // 
+            this.txtArrival.Location = new System.Drawing.Point(615, 12);
+            this.txtArrival.Name = "txtArrival";
+            this.txtArrival.Size = new System.Drawing.Size(173, 20);
+            this.txtArrival.TabIndex = 1;
+            this.txtArrival.Text = "8";
+            // 
+            // lblArrival
+            // 
+            this.lblArrival.AutoSize = true;
+            this.lblArrival.Location = new System.Drawing.Point(415, 12);
+            this.lblArrival.Name = "lblArrival";
+            this.lblArrival.Size = new System.Drawing.Size(197, 13);
+            this.lblArrival.TabIndex = 2;
+            this.lblArrival.Text = "No. of Minutes for each student to arrive";
+            // 
+            // lblLevel
+            // 
+            this.lblLevel.AutoSize = true;
+            this.lblLevel.Location = new System.Drawing.Point(415, 38);
+            this.lblLevel.Name = "lblLevel";
+            this.lblLevel.Size = new System.Drawing.Size(90, 13);
+            this.lblLevel.TabIndex = 4;
+            this.lblLevel.Text = "Number of Levels";
+            // 
+            // txtLevels
+            // 
+            this.txtLevels.Location = new System.Drawing.Point(615, 38);
+            this.txtLevels.Name = "txtLevels";
+            this.txtLevels.Size = new System.Drawing.Size(173, 20);
+            this.txtLevels.TabIndex = 3;
+            this.txtLevels.Text = "3";
+            // 
+            // lblLvlNum
+            // 
+            this.lblLvlNum.AutoSize = true;
+            this.lblLvlNum.Location = new System.Drawing.Point(415, 64);
+            this.lblLvlNum.Name = "lblLvlNum";
+            this.lblLvlNum.Size = new System.Drawing.Size(144, 13);
+            this.lblLvlNum.TabIndex = 6;
+            this.lblLvlNum.Text = "No. of Counters in each level";
+            // 
+            // txtLvlNum
+            // 
+            this.txtLvlNum.Location = new System.Drawing.Point(615, 64);
+            this.txtLvlNum.Name = "txtLvlNum";
+            this.txtLvlNum.Size = new System.Drawing.Size(173, 20);
+            this.txtLvlNum.TabIndex = 5;
+            this.txtLvlNum.Text = "1,3,4";
+            // 
+            // lblProcTime
+            // 
+            this.lblProcTime.AutoSize = true;
+            this.lblProcTime.Location = new System.Drawing.Point(415, 90);
+            this.lblProcTime.Name = "lblProcTime";
+            this.lblProcTime.Size = new System.Drawing.Size(157, 13);
+            this.lblProcTime.TabIndex = 8;
+            this.lblProcTime.Text = "Processing Time for Each Level";
+            // 
+            // txtProcTime
+            // 
+            this.txtProcTime.Location = new System.Drawing.Point(615, 90);
+            this.txtProcTime.Name = "txtProcTime";
+            this.txtProcTime.Size = new System.Drawing.Size(173, 20);
+            this.txtProcTime.TabIndex = 7;
+            this.txtProcTime.Text = "2,10,20";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(415, 224);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(98, 13);
+            this.lblTime.TabIndex = 10;
+            this.lblTime.Text = "Time Stamp Output";
+            // 
+            // txtTime
+            // 
+            this.txtTime.Location = new System.Drawing.Point(615, 224);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Size = new System.Drawing.Size(173, 20);
+            this.txtTime.TabIndex = 9;
+            this.txtTime.Text = "0";
+            // 
+            // lblPplInSys
+            // 
+            this.lblPplInSys.AutoSize = true;
+            this.lblPplInSys.Location = new System.Drawing.Point(415, 250);
+            this.lblPplInSys.Name = "lblPplInSys";
+            this.lblPplInSys.Size = new System.Drawing.Size(120, 13);
+            this.lblPplInSys.TabIndex = 12;
+            this.lblPplInSys.Text = "No. of People in System";
+            // 
+            // txtPplNum
+            // 
+            this.txtPplNum.Location = new System.Drawing.Point(615, 250);
+            this.txtPplNum.Name = "txtPplNum";
+            this.txtPplNum.Size = new System.Drawing.Size(173, 20);
+            this.txtPplNum.TabIndex = 11;
+            this.txtPplNum.Text = "0";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(615, 168);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(173, 50);
+            this.btnReset.TabIndex = 13;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnSimulate
+            // 
+            this.btnSimulate.Location = new System.Drawing.Point(418, 168);
+            this.btnSimulate.Name = "btnSimulate";
+            this.btnSimulate.Size = new System.Drawing.Size(173, 50);
+            this.btnSimulate.TabIndex = 14;
+            this.btnSimulate.Text = "Simulate";
+            this.btnSimulate.UseVisualStyleBackColor = true;
+            this.btnSimulate.Click += new System.EventHandler(this.btnSimulate_Click);
+            // 
+            // lblInitial
+            // 
+            this.lblInitial.AutoSize = true;
+            this.lblInitial.Location = new System.Drawing.Point(415, 116);
+            this.lblInitial.Name = "lblInitial";
+            this.lblInitial.Size = new System.Drawing.Size(96, 13);
+            this.lblInitial.TabIndex = 16;
+            this.lblInitial.Text = "System Initial State";
+            // 
+            // txtIniState
+            // 
+            this.txtIniState.Location = new System.Drawing.Point(615, 116);
+            this.txtIniState.Name = "txtIniState";
+            this.txtIniState.Size = new System.Drawing.Size(173, 20);
+            this.txtIniState.TabIndex = 15;
+            this.txtIniState.Text = "0;0,0,0;0,0,0,0";
+            // 
+            // lblStudentNum
+            // 
+            this.lblStudentNum.AutoSize = true;
+            this.lblStudentNum.Location = new System.Drawing.Point(415, 142);
+            this.lblStudentNum.Name = "lblStudentNum";
+            this.lblStudentNum.Size = new System.Drawing.Size(136, 13);
+            this.lblStudentNum.TabIndex = 18;
+            this.lblStudentNum.Text = "Number of students coming";
+            // 
+            // txtNumStudent
+            // 
+            this.txtNumStudent.Location = new System.Drawing.Point(615, 142);
+            this.txtNumStudent.Name = "txtNumStudent";
+            this.txtNumStudent.Size = new System.Drawing.Size(173, 20);
+            this.txtNumStudent.TabIndex = 17;
+            this.txtNumStudent.Text = "20";
+            // 
+            // lblQ
+            // 
+            this.lblQ.AutoSize = true;
+            this.lblQ.Location = new System.Drawing.Point(415, 276);
+            this.lblQ.Name = "lblQ";
+            this.lblQ.Size = new System.Drawing.Size(75, 13);
+            this.lblQ.TabIndex = 20;
+            this.lblQ.Text = "Queue Length";
+            // 
+            // txtQ
+            // 
+            this.txtQ.Location = new System.Drawing.Point(615, 276);
+            this.txtQ.Name = "txtQ";
+            this.txtQ.Size = new System.Drawing.Size(173, 20);
+            this.txtQ.TabIndex = 19;
+            this.txtQ.Text = "0";
+            // 
+            // lblStuYetCome
+            // 
+            this.lblStuYetCome.AutoSize = true;
+            this.lblStuYetCome.Location = new System.Drawing.Point(415, 354);
+            this.lblStuYetCome.Name = "lblStuYetCome";
+            this.lblStuYetCome.Size = new System.Drawing.Size(110, 13);
+            this.lblStuYetCome.TabIndex = 22;
+            this.lblStuYetCome.Text = "Students Yet to Come";
+            // 
+            // txtYet
+            // 
+            this.txtYet.Location = new System.Drawing.Point(615, 354);
+            this.txtYet.Name = "txtYet";
+            this.txtYet.Size = new System.Drawing.Size(173, 20);
+            this.txtYet.TabIndex = 21;
+            this.txtYet.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(615, 380);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 58);
+            this.button1.TabIndex = 23;
+            this.button1.Text = "Simulate";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btnSimulateOnce
+            // 
+            this.btnSimulateOnce.Location = new System.Drawing.Point(418, 380);
+            this.btnSimulateOnce.Name = "btnSimulateOnce";
+            this.btnSimulateOnce.Size = new System.Drawing.Size(173, 58);
+            this.btnSimulateOnce.TabIndex = 24;
+            this.btnSimulateOnce.Text = "Simulate 1 Step";
+            this.btnSimulateOnce.UseVisualStyleBackColor = true;
+            this.btnSimulateOnce.Click += new System.EventHandler(this.btnSimulateOnce_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(415, 302);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "Queue Length";
+            this.label1.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(615, 302);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(173, 20);
+            this.textBox1.TabIndex = 25;
+            this.textBox1.Text = "0";
+            this.textBox1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(415, 328);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "Queue Length";
+            this.label2.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(615, 328);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(173, 20);
+            this.textBox2.TabIndex = 27;
+            this.textBox2.Text = "0";
+            this.textBox2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnSimulateOnce);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblStuYetCome);
+            this.Controls.Add(this.txtYet);
+            this.Controls.Add(this.lblQ);
+            this.Controls.Add(this.txtQ);
+            this.Controls.Add(this.lblStudentNum);
+            this.Controls.Add(this.txtNumStudent);
+            this.Controls.Add(this.lblInitial);
+            this.Controls.Add(this.txtIniState);
+            this.Controls.Add(this.btnSimulate);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lblPplInSys);
+            this.Controls.Add(this.txtPplNum);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.txtTime);
+            this.Controls.Add(this.lblProcTime);
+            this.Controls.Add(this.txtProcTime);
+            this.Controls.Add(this.lblLvlNum);
+            this.Controls.Add(this.txtLvlNum);
+            this.Controls.Add(this.lblLevel);
+            this.Controls.Add(this.txtLevels);
+            this.Controls.Add(this.lblArrival);
+            this.Controls.Add(this.txtArrival);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtArrival;
+        private System.Windows.Forms.Label lblArrival;
+        private System.Windows.Forms.Label lblLevel;
+        private System.Windows.Forms.TextBox txtLevels;
+        private System.Windows.Forms.Label lblLvlNum;
+        private System.Windows.Forms.TextBox txtLvlNum;
+        private System.Windows.Forms.Label lblProcTime;
+        private System.Windows.Forms.TextBox txtProcTime;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.Label lblPplInSys;
+        private System.Windows.Forms.TextBox txtPplNum;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnSimulate;
+        private System.Windows.Forms.Label lblInitial;
+        private System.Windows.Forms.TextBox txtIniState;
+        private System.Windows.Forms.Label lblStudentNum;
+        private System.Windows.Forms.TextBox txtNumStudent;
+        private System.Windows.Forms.Label lblQ;
+        private System.Windows.Forms.TextBox txtQ;
+        private System.Windows.Forms.Label lblStuYetCome;
+        private System.Windows.Forms.TextBox txtYet;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnSimulateOnce;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ CountersQueuingProblemSolver/Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace CountersQueuingProblemSolver
+{
+    public partial class Form1 : Form
+    {
+        private int timeStamp = 0;
+        private CounterSystem cs;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSimulate_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Enabled = cs.Update();
+            txtTime.Text = timeStamp.ToString();
+            txtPplNum.Text = cs.getCount().ToString();
+            txtQ.Text = cs.getQbyLevel();
+            txtYet.Text = cs.getNumYet().ToString();
+            timeStamp++;
+       }
+
+        private int[] IntsFromStringArray(String[] array)
+        {
+            int[] ints = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                ints[i] = Convert.ToInt32(array[i]);
+            }
+            return ints;
+        }
+
+        private int[][] IntsArrayFromStringArray(String[] array)
+        {
+            int count = array.Length;
+            List<int[]> lint = new List<int[]>();
+            for (int i = 0; i < count; i++)
+            {
+                lint.Add(IntsFromStringArray(array[i].Split(',')));
+            }
+            return lint.ToArray();
+        }
+
+        private bool[][] BoolsArrFromIntsArr(int[][] input)
+        {
+            List<bool[]> lbool = new List<bool[]>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                int count = input[i].Length;
+                bool[] b = new bool[count];
+                for (int j = 0; j < count; j++)
+                {
+                    b[j] = input[i][j] == 0 ? false : true;
+                }
+                lbool.Add(b);
+            }
+            return lbool.ToArray();
+        }
+
+        private void Initialize()
+        {
+            timeStamp = 0;
+            int comingTime = Convert.ToInt32(txtArrival.Text);
+            int levelNum = Convert.ToInt32(txtLevels.Text);
+            int totalStudents = Convert.ToInt32(txtNumStudent.Text);
+            int[] levelCounters = IntsFromStringArray(txtLvlNum.Text.Split(','));
+            int[] levelProccessTime = IntsFromStringArray(txtProcTime.Text.Split(','));
+            int[][] systemInitialState = IntsArrayFromStringArray(txtIniState.Text.Split(';'));
+            bool[][] iniState = BoolsArrFromIntsArr(systemInitialState);
+            cs = new CounterSystem(levelNum, totalStudents, levelCounters, levelProccessTime, iniState, comingTime);
+            txtTime.Text = "0";
+            txtPplNum.Text = "0";
+            txtQ.Text = "0";
+            txtYet.Text = totalStudents.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Initialize();
+            txtPplNum.Text = cs.getCount().ToString();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            Initialize();
+        }
+
+        private void btnSimulateOnce_Click(object sender, EventArgs e)
+        {
+            cs.Update();
+            txtTime.Text = timeStamp.ToString();
+            txtPplNum.Text = cs.getCount().ToString();
+            txtQ.Text = cs.getQbyLevel();
+            txtYet.Text = cs.getNumYet().ToString();
+            timeStamp++;
+        }
+    }
+
+    public class Counter
+    {
+        private int processingTime;
+        private bool state;
+        private int countDown;
+        public Counter(int processingTime, bool state)
+        {
+            this.processingTime = processingTime;
+            this.state = state;
+            countDown = processingTime;
+        }
+        public void Update()
+        {
+            if (state)
+            {
+                if (countDown > 0)
+                {
+                    countDown--;
+                }
+                else
+                {
+                    countDown = processingTime;
+                    state = false;
+                }
+            }
+        }
+        public bool IsOccupied()
+        {
+            return state;
+        }
+        public void Assign()
+        {
+            state = true;
+            countDown = processingTime;
+        }
+    }
+
+    public class Level
+    {
+        private Counter[] counters;
+        private int queueNum;
+        private int leaving;
+        public Level(int counterCount, int processingTime, bool[] states)
+        {
+            bool[] iniStates = new bool[counterCount];
+            bool equalLength = states.Length == counterCount;
+            for (int i = 0; i < counterCount; i++)
+            {
+                iniStates[i] = equalLength ? states[i] : false;
+            }
+            counters = new Counter[counterCount];
+            for (int i = 0; i < counterCount; i++)
+            {
+                counters[i] = new Counter(processingTime, iniStates[i]);
+            }
+            queueNum = 0;
+            leaving = 0;
+        }
+        public int countPeople()
+        {
+            int rtn = 0;
+            for (int i = 0; i < counters.Length; i++)
+            {
+                if (counters[i].IsOccupied()) rtn++;
+            }
+            return rtn;
+        }
+        public void Update(int feedingNum)
+        {
+            int beforeUpdateInnerNum = countPeople();
+            foreach (Counter c in counters)
+            {
+                c.Update();
+            }
+            int afterUpdateInnerNum = countPeople();
+            leaving = beforeUpdateInnerNum - afterUpdateInnerNum;
+            int newCapacity = counters.Length - afterUpdateInnerNum;
+            queueNum += Math.Max(0, feedingNum - newCapacity);
+            int entering = feedingNum + queueNum > newCapacity ? newCapacity : feedingNum + queueNum;
+            queueNum = Math.Max(0, queueNum - entering);
+            for (int i = 0; i < counters.Length; i++)
+            {
+                if (counters[i].IsOccupied()) continue;
+                if (entering > 0)
+                {
+                    counters[i].Assign();
+                    entering--;
+                }
+            }
+        }
+        public int getQueue()
+        {
+            return queueNum;
+        }
+        public int getLeaving()
+        {
+            return leaving;
+        }
+    }
+    public class CounterSystem
+    {
+        private int comingTime;
+        private int countDown;
+        private int queuing;
+        private int numStudent;
+        private List<Level> levels;
+        public CounterSystem(int levels, int students, int[] levelNumCounters, int[] levelProcessingTime, bool[][] levelStates, int peopleComingTime)
+        {
+            comingTime = peopleComingTime;
+            countDown = comingTime;
+            numStudent = students;
+            queuing = 0;
+            this.levels = new List<Level>();
+            for (int i = 0; i < levels; i++)
+            {
+                Level level = new Level(levelNumCounters[i], levelProcessingTime[i], levelStates[i]);
+                this.levels.Add(level);
+            }
+        }
+        public bool Update()
+        {
+            if (countDown > 0)
+            {
+                countDown--;
+            }
+            else
+            {
+                countDown = comingTime;
+                if (numStudent > 0)
+                {
+                    queuing++;
+                    numStudent--;
+                }　
+            }
+            int levelOutput = queuing;
+            int sum = 0;
+            for (int i = 0; i < levels.Count; i++)
+            {
+                levels[i].Update(levelOutput);
+                levelOutput = levels[i].getLeaving();
+                if (i == 0) queuing = levels[i].getQueue();
+                sum += levels[i].countPeople();
+            }
+            return sum > 0 || numStudent > 0;
+        }
+        public int getCount()
+        {
+            int sum = 0;
+            for (int i = 0; i < levels.Count; i++)
+            {
+                sum += levels[i].countPeople();
+            }
+            return sum;
+        }
+        public int getQ()
+        {
+            return queuing;
+        }
+        public String getQbyLevel()
+        {
+            int index = 1;
+            String rtn = "";
+            for (int i = 0; i < levels.Count; i++)
+            {
+                rtn += "Lvl" + index.ToString() +"-[";
+                rtn += levels[i].getQueue().ToString();
+                rtn += "]; ";
+                index++;
+            }
+            return rtn;
+        }
+        public int getNumYet()
+        {
+            return numStudent;
+        }
+    }
+}
